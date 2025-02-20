dd7c709ad452a8dadc50177a6a15cc7177ede8a5
Nerii10 <96701056+Nerii10@users.noreply.github.com>
2025-02-20 15:32:29 +0100

    e

--- src/assets/Misc/ToggleButton.jsx
+++ src/assets/Misc/ToggleButton.jsx
@@ -1,45 +1,46 @@
-
-
 const ListOpenSignTop = {
     marginTop: "5px",
     position: "absolute",
-    transform: "scaleX(3)",
+    transform: "scale(3, 1)",
+    color:"white",
+    opacity:1,
     transition: "0.5s ease",
 }
 
 const ListOpenSignBottom = {
     marginTop: "-5px",
+    color:"white",
+    opacity:1,
     position: "absolute",
-    transform: "scaleX(3)",
+    transform: "scale(3, 1)",
     transition: "0.5s ease",
 }
 
 const ListClosedSignBottom = {
-    rotate: "-45deg",
     marginLeft: "0px",
     position: "absolute",
-    transform: "scaleX(3)",
+    color:"white",
+    opacity:1,
+    transform: "rotate(-45deg) scale(3, 1)",
     transition: "0.5s ease",
 }
 
 const ListClosedSignTop = {
-    rotate: "45deg",
-  
     marginLeft: "3px",
     marginTop: "-2px",
+    color:"white",
+    opacity:1,
     position: "absolute",
-    transform: "scaleX(3)",
+    transform: "rotate(45deg) scale(3, 1)",
     transition: "0.5s ease",
 }
 
-
-export default function ToggleButton({active}) {
-    return(
-        <>
-            <h2 style={{ margin: 0,display:'flex', flexDirection:"column", cursor:"pointer" }} >
-                <span style={{opacity:0}}>-</span>
-                <span style={active ? ListOpenSignBottom : ListClosedSignBottom}>-</span><span style={active ? ListOpenSignTop : ListClosedSignTop}>-</span>
-            </h2>
-        </>
+export default function ToggleButton({ active }) {
+    return (
+        <h2 style={{ margin: 0, display: 'flex', flexDirection: "column", cursor: "pointer" }} >
+            <span style={{ opacity: 0 }}>-</span>
+            <span style={active ? ListOpenSignBottom : ListClosedSignBottom}>-</span>
+            <span style={active ? ListOpenSignTop : ListClosedSignTop}>-</span>
+        </h2>
     )
 }
